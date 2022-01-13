1721dc3c3be9e32c049071395e1970077dcf2726
csyoon <chiseon0819@gmail.com>
2022-01-13 13:45:35 +0900

    첫번째 커밋

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Pressable, TouchableOpacity } from "react-native";
+import styled, { ThemeProvider } from "styled-components/native";
+import { theme } from "./theme";
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.bg};
+  padding-horizontal: 20;
+`;
+
+const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 100;
+`;
+
+const HeaderText = styled.Text`
+  color: ${({ theme }) => theme.white};
+  font-size: 38;
+  font-weight: 600;
+`;
 
 export default function App() {
+  const [working, setWorking] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StatusBar style="auto" />
+        <Header>
+          <Pressable>
+            <HeaderText>Work</HeaderText>
+          </Pressable>
+          <HeaderText>Traval</HeaderText>
+        </Header>
+      </Container>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
